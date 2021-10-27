62cf3557c312d478c61c07301d6035bd9b6155fc
root <root@dev.viral.def24.com>
2021-10-27 20:46:48 +0000

    basics

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include "header.php"; ?>
+<h1>Main</h1>
+<a href='second.php'>Go to Second Page</a>
+<?php include "footer.php"; ?>
---
+++ second.php
@@ -0,0 +1,4 @@
+<?php include "header.php"; ?>
+<h1>I am second page</h1>
+<a href='index.php'>Go to Index</a>
+<?php include "footer.php"; ?>
